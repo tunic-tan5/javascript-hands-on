c8b299dc85b83f8cc4c55ed255f30760cd5cdb4d
TANMAI GARA <tanmaigara@gmail.com>
2026-01-21 21:55:59 +0530

    Add files via upload

---
+++ date2.js
@@ -0,0 +1,26 @@
+//assignment 2 data comparision and validation
+/*Assignment 2: Date Comparison & Validation (Beginner → Intermediate)
+--------------------------------------------------------------------
+
+ Given:
+      let enrollmentDeadline = new Date("2026-01-20");
+
+Tasks:
+  1.Check if:
+      * Today is before deadline → "Enrollment Open"
+      * Today is after deadline → "Enrollment Closed"
+
+  2. Validate user input date:
+      * Input: "2026-02-30"
+      * Detect whether the date is valid or invalid*/
+
+let enrollmentDeadline = new Date("2026-02-20");
+let today = new Date();
+
+if (today < enrollmentDeadline) {
+    console.log("Enrollment Open");
+} else {
+    console.log("Enrollment Closed");
+}
+ 
+  
---
+++ deep_copy.js
@@ -0,0 +1,24 @@
+/*Hands-On 2: Deep Copy (Isolation & Safety Use Case)
+---------------------------------------------------
+
+🧪 Given Data:
+                const order = {
+                  orderId: "ORD1001",
+                  customer: {
+                    name: "Anita",
+                    address: {
+                      city: "Hyderabad",
+                      pincode: 500085
+                    }
+                  },
+                  items: [
+                    { product: "Laptop", price: 70000 }
+                  ]
+                };
+
+🎯 Task:
+      1. Create a deep copy of order
+      2. Modify in copied object:
+            i. customer.address.city
+            ii. items[0].price
+            iii. Verify original object remains unchanged*/
