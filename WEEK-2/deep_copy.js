94fd0b97ef453704e48c8847c101bdda20cbda54
TANMAI GARA <tanmaigara@gmail.com>
2026-01-30 19:47:19 +0530

    Update deep_copy.js

--- WEEK-2/deep_copy.js
+++ WEEK-2/deep_copy.js
@@ -22,3 +22,27 @@
             i. customer.address.city
             ii. items[0].price
             iii. Verify original object remains unchanged*/
+const order = {
+    orderId: "ORD1001",
+    customer: {   
+      name: "Anita",
+      address: {
+        city: "Hyderabad",
+        pincode: 500085
+      }
+    },
+    items: [
+      { product: "Laptop", price: 70000 }
+    ]
+  };
+  // Creating a deep copy of the order object
+  const deepCopiedOrder = JSON.parse(JSON.stringify(order));    
+  // Modifying the deep copied object
+  deepCopiedOrder.customer.address.city = "Bangalore";  
+  deepCopiedOrder.items[0].price = 65000;
+  // Verifying the original object remains unchanged
+  console.log("Original Order:", order);  
+  console.log("Modified Deep Copied Order:", deepCopiedOrder);
+
+
+  
